107f4695b906f1bed53d3d9344acedafcd61a8cc
ian <ianwisdom90@gmail.com>
2025-03-22 15:03:37 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor="#000000";
+dodger.style.backgroundColor="#FF69B4";
+dodger.style.bottom="0px";
+dodger.style.left="0px";
+
+document.addEventListener("keydown", function(event){
+    console.log(event);
+});
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    const gameWidth = 400;
+  const dodgerWidth = 40;
+
+  if (left < gameWidth - dodgerWidth) {
+    dodger.style.left = `${left + 10}px`;
+  }
+  }
